779ca65a5a8fab8344512d10134b0381ecfbfd28
Elliot Sayes <elliot@sayes.cloud>
2023-09-13 14:59:47 +0700

    Fix typo

--- src/lib/convert.ts
+++ src/lib/convert.ts
@@ -10,7 +10,7 @@ const extractGarItems = (
   return Object.entries(garCache.gateways).map(([txId, item]) => ({
     id: txId,
     ping: { status: "unknown" },
-    link: `${item.settings.protocol}://${item.settings.fqdn}:${item.settings.port}/}`,
+    link: `${item.settings.protocol}://${item.settings.fqdn}:${item.settings.port}/`,
     ...item,
   }));
 };
