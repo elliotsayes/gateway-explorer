f81126f540e8f044edd0cd555756c4cba8ac53df
Elliot Sayes <elliot@sayes.cloud>
2023-09-13 12:51:59 +0700

    RefreshButton

---
+++ src/components/RefreshButton.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { RefreshCcw } from "lucide-react";
+
+interface Props {
+  isRefreshing: boolean
+  onClick: () => void
+}
+
+const RefreshButton = ({ isRefreshing, onClick }: Props) => {
+  return (
+    <Button onClick={onClick} disabled={isRefreshing} size={"icon"}>
+      <RefreshCcw className={`h-4 w-4 ${isRefreshing && 'animate-spin-ccw'}`} />
+    </Button>
+  );
+}
+
+export { RefreshButton }
--- tailwind.config.js
+++ tailwind.config.js
@@ -65,10 +65,15 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        "spin-ccw": {
+          from: { transform: "rotate(360deg)" },
+          to: { transform: "rotate(0deg)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "spin-ccw": "spin-ccw 1s linear infinite",
       },
     },
   },
