4ee8ec391ae73ed450ee2ac22f97cbdfc34010bb
Elliot Sayes <elliot@sayes.cloud>
2023-11-12 20:55:04 +0700

    dont display timing

--- src/components/GarTable.tsx
+++ src/components/GarTable.tsx
@@ -215,7 +215,7 @@ const GarTable = ({ data, onRefresh, isRefreshing, onItemUpdate, onItemSelect, s
               (
                 <div className="flex flex-row items-center gap-2 max-w-[10rem]">
                   <div className="h-2 w-2 rounded-full bg-green-400" />
-                  <span className="text-xs text-muted-foreground line-clamp-1">Pass ({cell.row.original.observation.result.resolution?.timings?.phases.request}ms)</span>
+                  <span className="text-xs text-muted-foreground line-clamp-1">Pass</span>
                   {button(true)}
                 </div>
               ) : (
