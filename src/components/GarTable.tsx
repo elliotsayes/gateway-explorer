4f84f7404a031f77921b0c682348b627e3f46df0
Elliot Sayes <elliot@sayes.cloud>
2023-11-12 20:46:27 +0700

    Timing

--- src/components/GarTable.tsx
+++ src/components/GarTable.tsx
@@ -178,7 +178,7 @@ const GarTable = ({ data, onRefresh, isRefreshing, onItemUpdate, onItemSelect, s
     {
       id: "Observation",
       accessorKey: "observation.status",
-      header: "Observation",
+      header: "Quick Observation",
       size: 20,
       cell: (cell) => {
         const status = cell.row.original.observation.status;
@@ -213,9 +213,9 @@ const GarTable = ({ data, onRefresh, isRefreshing, onItemUpdate, onItemSelect, s
           case "success":
             return (cell.row.original.observation.result.pass) ? 
               (
-                <div className="flex flex-row items-center gap-2 max-w-[8rem]">
+                <div className="flex flex-row items-center gap-2 max-w-[10rem]">
                   <div className="h-2 w-2 rounded-full bg-green-400" />
-                  <span className="text-xs text-muted-foreground line-clamp-1 text-clip">Pass</span>
+                  <span className="text-xs text-muted-foreground line-clamp-1">Pass ({cell.row.original.observation.result.resolution?.timings?.phases.request}ms)</span>
                   {button(true)}
                 </div>
               ) : (
