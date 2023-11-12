5dbc7fcd5e0356764d5ab1ec322dbb638eed5ea4
Elliot Sayes <elliot@sayes.cloud>
2023-11-12 15:28:06 +0700

    Change back app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-// import GarLoader from './components/GarLoader'
-import { LoadTest } from './lib/observer/LoadTest'
+import GarLoader from './components/GarLoader'
 
 const queryClient = new QueryClient()
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <GarLoader /> */}
-      <LoadTest />
+      <GarLoader />
     </QueryClientProvider>
   )
 }
