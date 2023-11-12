bf1e18d19ccddc3eac2fb53a7c563106b424454a
Elliot Sayes <elliot@sayes.cloud>
2023-11-12 20:41:29 +0700

    clear duplicated types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { arnsResolutionSchema } from "./lib/observer/schema";
 
 export const zArweaveTxId = z.string().regex(/^[\w-]{43}$/);
 
@@ -36,42 +37,10 @@ export const zGatewayHealthCheck = z.object({
   date: z.string().datetime().optional(),
 });
 
-export const zArnsResolutionSuccess = z.object({
-  statusCode: z.literal(200),
-  resolvedId: z.string(),
-  ttlSeconds: z.string(),
-  contentType: z.string(),
-  contentLength: z.string().nullable(),
-  dataHashDigest: z.string(),
-  timings: z.object({
-    wait: z.number().int().nonnegative().optional(),
-    dns: z.number().int().nonnegative().optional(),
-    tcp: z.number().int().nonnegative().optional(),
-    tls: z.number().int().nonnegative().optional(),
-    request: z.number().int().nonnegative().optional(),
-    firstByte: z.number().int().nonnegative().optional(),
-    download: z.number().int().nonnegative().optional(),
-    total: z.number().int().nonnegative(),
-  }),
-});
-
-export const zArnsResolution = z.discriminatedUnion("statusCode", [
-  zArnsResolutionSuccess,
-  z.object({
-    statusCode: z.literal(404),
-    resolvedId: z.null(),
-    ttlSeconds: z.null(),
-    contentType: z.null(),
-    contentLength: z.null(),
-    dataHashDigest: z.null(),
-    timings: z.null(),
-  }),
-]);
-
 // TODO: Copy from observation protocol standard
 export const zArioObservation = z.object({
   pass: z.boolean(),
-  resolution: zArnsResolution.optional(),
+  resolution: arnsResolutionSchema.optional(),
 });
 
 export const zGatewayAddressRegistryItem = z.intersection(
