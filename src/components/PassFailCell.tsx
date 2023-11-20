6f9d402d7c4e7a2f2dd45efef115ad1af8ab47c4
Elliot Sayes <elliot@sayes.cloud>
2023-11-20 12:01:09 +0700

    Tweak spacing

--- src/components/PassFailCell.tsx
+++ src/components/PassFailCell.tsx
@@ -6,15 +6,15 @@ interface PassFailCellProps {
 export const PassFailCell = ({ pass, children }: PassFailCellProps) => {
   return (
     pass ? (
-      <div className="flex flex-row items-center gap-2 max-w-[10rem]">
+      <div className="flex flex-row items-center max-w-[10rem] gap-1">
         <div className="h-2 w-2 rounded-full bg-green-400" />
-        <span className="text-xs text-muted-foreground line-clamp-1">Pass</span>
+        <span className="ml-1 text-xs text-muted-foreground line-clamp-1">Pass</span>
         {children}
       </div>
     ) : (
-      <div className="flex flex-row items-center gap-2 max-w-[8rem]">
+      <div className="flex flex-row items-center max-w-[8rem] gap-1">
         <div className="h-2 w-2 rounded-full bg-red-400" />
-        <span className="text-xs text-muted-foreground line-clamp-1 text-clip">Fail</span>
+        <span className="ml-1 text-xs text-muted-foreground line-clamp-1">Fail</span>
         {children}
       </div>
     )
