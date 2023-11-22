aa780c09dad78928741ed408579d2761d38d79ef
Elliot Sayes <elliot@sayes.cloud>
2023-11-22 13:03:40 +0700

    Remove debug logs

--- src/LazyObserve.tsx
+++ src/LazyObserve.tsx
@@ -3,7 +3,6 @@ import { observeIndexRoute } from "./routes"
 import { ObservationListSingleGateway } from "./components/ObservationListSingleGateway"
 
 export const ObserveIndex = () => {
-  console.log("Rendering ObserveIndex")
   const params = useParams({ from: observeIndexRoute.id })
   return <ObservationListSingleGateway host={params.host} />
 }
--- src/components/ObservationListSingleGateway.tsx
+++ src/components/ObservationListSingleGateway.tsx
@@ -3,7 +3,6 @@ interface Props {
 }
 
 export const ObservationListSingleGateway = ({ host }: Props) => {
-  console.log("Rendering ObservationListSingleGateway")
   return (
     <div>
       <p>ObservationListSingleGateway: {host}</p>
