dc4f6370eed6c4b89109fa838a05f808cdf44e10
Elliot Sayes <elliot@sayes.cloud>
2024-02-01 23:20:09 +0800

    Format

--- src/components/DistributionDetails.tsx
+++ src/components/DistributionDetails.tsx
@@ -1,6 +1,8 @@
 import { useNetwork } from "@/hooks/useNetwork";
 import { fetchDistributions } from "@/lib/distribution/fetchDistributions";
 import { useQuery } from "@tanstack/react-query";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
+import { InfoIcon } from "lucide-react";
 
 const DistributionDetails = () => {
   const { network } = useNetwork();
@@ -14,7 +16,24 @@ const DistributionDetails = () => {
   return (
     <div>
       <p className="text-lg">Distributions for epoch {distributionsData.epochPeriod}</p>
-      <div>{JSON.stringify(distributionsData, undefined, 2)}</div>
+      <div>
+        <div className="flex flex-row gap-1">
+          <span>
+            Height: {distributionsData.epochStartHeight} - {distributionsData.epochEndHeight}
+          </span>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <InfoIcon size={12} />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Zero epoch height: {distributionsData.epochZeroStartHeight}</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+        <p>Next distribution at {distributionsData.nextDistributionHeight}</p>
+      </div>
     </div>
   )
 }
