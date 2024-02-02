cd6efbc934ffd0045126a5c70caa5809fd461a89
Elliot Sayes <elliot@sayes.cloud>
2024-02-02 23:47:53 +0800

    Disable urlDerivedNetwork

--- src/hooks/useNetwork.tsx
+++ src/hooks/useNetwork.tsx
@@ -1,15 +1,20 @@
 import { Network, defaultNetwork } from '@/lib/networks';
 import { useLocalStorage } from '@rehooks/local-storage';
 
-const url = new URL(window.location.href);
-const hostname = url.hostname;
-const domainParts = hostname.split('.');
-const tld = domainParts.slice(-1)[0];
-
-let defaultNetworkContextual: Network = import.meta.env.VITE_DEFAULT_NETWORK ?? defaultNetwork;
-if (tld === 'dev') {
-  defaultNetworkContextual = 'devnet';
+// @ts-expect-error For future use
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const urlDerivedNetwork = () => {
+  const url = new URL(window.location.href);
+  const hostname = url.hostname;
+  const domainParts = hostname.split('.');
+  const tld = domainParts.slice(-1)[0];
+  if (tld === 'dev') {
+    return 'devnet';
+  }
+  return null;
 }
+
+const defaultNetworkContextual: Network = import.meta.env.VITE_DEFAULT_NETWORK ?? defaultNetwork;
 
 export const useNetwork = () => {
   const [network, setNetwork] = useLocalStorage<Network>('network', defaultNetworkContextual);
